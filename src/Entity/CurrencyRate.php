3af0a0b5413c16d57ad58442a8122dfc9ed1b6e3
Ferzender Varli <fvarlii@gmail.com>
2023-05-28 13:50:21 +0300

    CurrencyRate description update

--- src/Entity/CurrencyRate.php
+++ src/Entity/CurrencyRate.php
@@ -6,6 +6,8 @@ use Doctrine\ORM\Mapping as ORM;
 /**
  * @ORM\Entity(repositoryClass="App\Repository\CurrencyRateRepository")
  * @ORM\Table(name="currency_rates")
+ * 
+ * This entity class represents a record of a currency exchange rate.
  */
 class CurrencyRate
 {
@@ -13,48 +15,51 @@ class CurrencyRate
      * @ORM\Id
      * @ORM\GeneratedValue
      * @ORM\Column(type="integer")
+     * 
+     * The unique identifier for this record.
      */
     private $id;
 
     /**
      * @ORM\Column(type="string", length=3)
+     * 
+     * The ISO code of the currency.
      */
     private $currency;
 
     /**
      * @ORM\Column(type="decimal", precision=10, scale=4)
+     * 
+     * The exchange rate of the currency against the base currency.
      */
     private $rate;
 
     // add getter and setter methods for properties
 
     public function getId(): ?int
-{
-    return $this->id;
-}
+    {
+        return $this->id;
+    }
 
-public function getCurrency(): ?string
-{
-    return $this->currency;
-}
+    public function getCurrency(): ?string
+    {
+        return $this->currency;
+    }
 
-public function setCurrency(string $currency): self
-{
-    $this->currency = $currency;
+    public function setCurrency(string $currency): self
+    {
+        $this->currency = $currency;
+        return $this;
+    }
 
-    return $this;
-}
+    public function getRate(): ?string
+    {
+        return $this->rate;
+    }
 
-public function getRate(): ?string
-{
-    return $this->rate;
-}
-
-public function setRate(string $rate): self
-{
-    $this->rate = $rate;
-
-    return $this;
-}
-
+    public function setRate(string $rate): self
+    {
+        $this->rate = $rate;
+        return $this;
+    }
 }
